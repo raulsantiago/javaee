72a1a771b50465a270a93e4b76218dd86b7bc406
raulsantiago <raulsantiago@id.uff.br>
2020-06-29 22:35:59 -0300

    Parte 2 Aula 2 - Carrinho de compras

---
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -0,0 +1,45 @@
+package br.com.casadocodigo.loja.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+// Named se torna um Bean do CDI
+@Named
+@SessionScoped
+public class CarrinhoCompras implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Set<CarrinhoItem> itens = new HashSet<>();
+
+    public void add(CarrinhoItem item) {
+        itens.add(item);
+    }
+
+	public List<CarrinhoItem> getItens() {
+		return new ArrayList<CarrinhoItem>(itens);
+	}
+	
+	public BigDecimal getTotal(CarrinhoItem item) {
+	    return item.getLivro().getPreco().multiply(new BigDecimal(item.getQuantidade()));
+	}
+	
+	
+	public BigDecimal getTotal() {
+		BigDecimal total = BigDecimal.ZERO;
+		for (CarrinhoItem carrinhoItem : itens) {
+			total = total.add(carrinhoItem.getLivro().getPreco().multiply(new BigDecimal(carrinhoItem.getQuantidade())));
+		}
+		return total;
+		
+	}
+	
+
+}
